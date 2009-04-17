55aa01637b25d41d12e3a86d6519e42d730c0330
bryancole <230838+bryancole@users.noreply.github.com>
2009-04-17 17:43:59 +0100

    added Input trait to ModelUpdate

--- src/TraitsModelUpdate.py
+++ src/TraitsModelUpdate.py
@@ -1,17 +1,25 @@
-from enthought.traits.api import (HasTraits, Property, Bool, TraitType,
-                                  TraitHandler, Tuple, on_trait_change,
-                                  Proprty, cached_property
+from enthought.traits.api import (HasTraits, Property, Bool, Tuple, 
+                        on_trait_change, cached_property, Instance,
+                        Float
                                   )
 
 
 from OCC import TDF, TopoDS
 
+    
+Input = Instance(klass="ProcessObject", process_input=True)
+
+
 class ProcessObject(HasTraits):
     modified = Bool
     
-    shape = Property(TopoDS.TopoDS_Shape, depends_on="modified")
+    label = Instance(TDF.TDF_Label)
+    
+    shape = Property(Instance(TopoDS.TopoDS_Shape), depends_on="modified")
       
-    def on_input_change(self, vold, vnew):
+    @on_trait_change("+process_input")
+    def on_input_change(self, obj, name, vold, vnew):
+        print "ch", vold, vnew
         if vold is not None:
             vold.on_trait_change(self.on_modify, 'modified', remove=True)
         
@@ -29,15 +37,28 @@ class ProcessObject(HasTraits):
         
     def execute(self):
         """return a TopoDS_Shape object"""
+        raise NotImplementedError
         
-
-Input = Trait(ProcessObject)
 
       
 class BlockSource(ProcessObject):
     dims = Tuple(10.0,20.0,30.0)
     
-    label = Instance(TDF.TDF_Label)
-    
     def _dims_changed(self):
         self.modified = True
+        
+class SphereSource(ProcessObject):
+    radius = Float(5.0)
+        
+class CutFilter(ProcessObject):
+    input = Input
+    tool = Input
+        
+if __name__=="__main__":
+    
+    c1 = CutFilter()
+    c2 = CutFilter()
+    c3 = CutFilter(input=c1, tool=c2)
+    
+    print c3.shape
+    
